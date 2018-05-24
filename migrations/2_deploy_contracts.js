bcec961375188aeef9afe385aaa55ea47b0611ff
David Akers <david@quintify.com>
2018-05-24 08:43:31 -0400

    update with mainnet log

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var hlthDRS = artifacts.require("./HealthDRS.sol")
-var hlthToken = '0xab6cee678340a12ee72d41d472300f6a2befa1eb' //update before deploy
+var hlthToken = '0xa17b6235ef312c7b3e281059006324460f3875fd' //update before deploy
 
 module.exports = function(deployer) {
     hlthDRS.new(hlthToken).then(function(instance) {
