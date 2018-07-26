8f6c4b026519d6f8106a93ea7c4633675d4405a5
Lucas Hendren <lhendre2@gmail.com>
2018-07-26 00:04:15 -0400

    Added in tests and got base case for the decentralized search running

--- test/helpers/HealthCashMock.sol
+++ test/helpers/HealthCashMock.sol
@@ -6,12 +6,26 @@ contract HealthCashMock is StandardToken {
 
   string public name = "Health Cash";
   string public symbol = "HLTH";
+  address public owner = 0x0000000000000000000000000000000000000000000000000000000000000000;
+
   uint256 public decimals = 18;
   uint256 public INITIAL_SUPPLY = 100000000;
 
    constructor() {
     totalSupply_ = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
+    owner=msg.sender;
   }
+
+
+  function getOwner() public constant returns (address) {
+      return owner;
+    }
+
+
+  function getBalance() public constant returns (uint256) {
+      return balances[msg.sender];
+    }
+
 
 }
