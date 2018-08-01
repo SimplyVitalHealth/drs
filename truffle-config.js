4b274679121367252ccaf3a6f7dc9b45b15de717
David Michael Akers <david@simplyvitalhealth.com>
2018-08-01 14:57:15 -0400

    updated config

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,13 @@
 module.exports = {
   migrations_directory: "./migrations",
+  solc: { optimizer: { enabled: true, runs: 200 } },  
   networks: {
-     solc: { optimizer: { enabled: true, runs: 200 } },  
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*",
-      from: "0x1ab1d852af5f4e0d5f202905ee5047eeefa5b8c3",
-     gas: 4712388
-
-
+      //from: "0x79cc456feb34cdc4af9c1281ee4be62940a215f5",
+      gas: 4712388 
     }
   }
 };
--- truffle.js
+++ truffle.js
@@ -3,16 +3,14 @@ require('babel-polyfill');
 
 module.exports = {
   migrations_directory: "./migrations",
+  solc: { optimizer: { enabled: true, runs: 200 } },
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*",
-      // gas: 10000000,
-      // gasPrice: 0x01,
-
-      from: "0x1ab1d852af5f4e0d5f202905ee5047eeefa5b8c3",
-
+      //from: "0x79cc456feb34cdc4af9c1281ee4be62940a215f5",
+      gas: 4712388   
     }
   }
 };
